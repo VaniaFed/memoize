188ef06f8cf5cbee79a73f8566b2c84304457998
Ivan Fediakov <ivan_fediakov@epam.com>
2019-06-25 12:31:52 +0300

    implemented memoize (5/12 tests pass)

--- src/memoize.js
+++ src/memoize.js
@@ -1,3 +1,15 @@
-export function memoize() {
-    // TODO
+export function memoize(f, resolver) {
+    if (typeof f === 'undefined' || typeof f !== 'function') {
+        return null;
+    }
+    const cache = new WeakMap();
+    return function memoized(...args) {
+        const key = resolver ? resolver.apply(this, args) : args[0];
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+        const result = f.apply(this, args);
+        memoized.cache = cache.set(key, result) || cache;
+        return result;
+    };
 }
